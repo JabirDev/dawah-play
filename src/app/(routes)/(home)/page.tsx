92efa908aceac7f833b04e858f9faab183447807
cakfan <taufan.fhm8@gmail.com>
2024-08-04 14:28:39 +0700

    fix: remove unused file

--- src/app/(routes)/(home)/page.tsx
+++ src/app/(routes)/(home)/page.tsx
@@ -1,18 +1,12 @@
-import { getAudio } from "@/actions/yt/getAudio";
-import SectionChannel from "@/components/section/channel";
-import SectionVidoes from "@/components/section/vidoes";
 import { Suspense } from "react";
-import Client from "./compoents/client";
+import SectionChannel from "@/components/section/channel";
 
-export default async function Home() {
-  const audio = await getAudio("wgb0oOuQ3NY");
-  //   console.log("audio:", audio);
+export default function Home() {
   return (
     <div className="flex min-h-screen w-full flex-col gap-2 px-2 py-6 md:px-4 lg:py-6">
       <Suspense fallback={<p>Loading...</p>}>
         <SectionChannel />
       </Suspense>
-      {audio && <Client {...audio} />}
       {/* <Suspense fallback={<p>Loading...</p>}>
         <SectionVidoes />
       </Suspense> */}
