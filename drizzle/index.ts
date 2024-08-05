246708f264c96a245d73c481a871d24168113d8e
cakfan <taufan.fhm8@gmail.com>
2024-08-05 17:51:58 +0700

    chore: change db url

--- drizzle/index.ts
+++ drizzle/index.ts
@@ -3,5 +3,5 @@ import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
 
 // const pool = new pg.Pool();
-const client = postgres(process.env.DATABASE_URL!);
+const client = postgres(process.env.DIRECT_URL!);
 export const db = drizzle(client);
